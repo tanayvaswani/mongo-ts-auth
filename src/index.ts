da1bf108a8763db2a1cd3b176e1f73905cb76425
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-30 18:11:10 +0530

    feat: auth router & controller skeleton

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import authRouter from "./routes/auth-router";
+
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/routes/auth-router.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+  registerUser,
+  authenticateUser,
+  logoutUser,
+} from "../controllers/auth-controller";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", authenticateUser);
+router.post("/logout", logoutUser);
+
+export default router;
