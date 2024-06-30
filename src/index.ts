d897c2dccac4d5ab5ed4a9dafe4677bf6e4e0253
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-30 18:22:55 +0530

    feat: dotenv and connectDB test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
 
 import authRouter from "./routes/auth-router";
+import connectDB from "./connections/database";
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+dotenv.config();
+connectDB();
 
 app.use(authRouter);
 
