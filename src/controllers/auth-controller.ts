71f523a95d969caa607854929661d26206f16d2c
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-30 18:21:47 +0530

    feat: Auth Controller & ConnectDB function

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,9 +1,58 @@
 import { Request, Response } from "express";
 
-const registerUser = (req: Request, res: Response) => {};
+import { generateToken, clearToken } from "../utils/auth";
+import User from "../models/user-model";
 
-const authenticateUser = (req: Request, res: Response) => {};
+const registerUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
 
-const logoutUser = (req: Request, res: Response) => {};
+  if (userExists) {
+    res.status(400).json({ message: "The user already exists" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id as string);
+
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res
+      .status(400)
+      .json({ message: "An error occurred while registering the user" });
+  }
+};
+
+const authenticateUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && user.comparePassword(password)) {
+    generateToken(res, user._id as string);
+
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401).json({ message: "User not found OR password incorrect" });
+  }
+};
+
+const logoutUser = (req: Request, res: Response) => {
+  clearToken(res);
+
+  res.status(200).json({ message: "User logged out" });
+};
 
 export { registerUser, authenticateUser, logoutUser };
---
+++ src/utils/auth.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+const generateToken = (res: Response, userId: string) => {
+  const jwtSecret = process.env.JWT_SECRET || "";
+  const token = jwt.sign({ userId }, jwtSecret, {
+    expiresIn: "1h",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000,
+  });
+};
+
+const clearToken = (res: Response) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+};
+
+export { generateToken, clearToken };
