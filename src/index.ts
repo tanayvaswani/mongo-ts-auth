3b0c614967687a2db21c6affea02e0d2f5904342
tanayvaswani <vaswani.tanay9@gmail.com>
2024-06-30 19:10:05 +0530

    feat: express.json()

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,15 @@ import dotenv from "dotenv";
 import express from "express";
 
 import authRouter from "./routes/auth-router";
+import { authenticate } from "./middlewares/auth-middleware";
 import connectDB from "./connections/database";
 import { errorHandler } from "./middlewares/error-handler";
+import userRouter from "./routes/user-router";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 dotenv.config();
-connectDB();
-
-// NOTE: Use this only if you are deploying
-// and if you wanna make this acccessible via frontend.
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     credentials: true,
-//   })
-// );
-
-app.use(authRouter);
-app.use(cookieParser());
-app.use(errorHandler);
 
 export interface UserBasicInfo {
   _id: string;
@@ -39,6 +27,23 @@ declare global {
     }
   }
 }
+
+// NOTE: Use this only if you are deploying
+// and if you wanna make this acccessible via frontend.
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+//   })
+// );
+
+app.use(express.json());
+app.use(authRouter);
+app.use(cookieParser());
+app.use("/users", authenticate, userRouter);
+app.use(errorHandler);
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
